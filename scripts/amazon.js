794d0a8491de9b1f0c0c51ce11b8ee1cc636705a
Your Name <22120427@student.hcmus.edu.vn>
2026-06-08 21:38:02 +0700

    feat: move addToCart method into cart.js and matching product order to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,5 @@
-import { cart } from '../data/cart.js';
+import { cart, addToCart} from "../data/cart.js";
+import { products } from "../data/products.js";
 
 let productsHTML = "";
 
@@ -55,38 +56,22 @@ products.forEach((product) => {
     `;
 });
 
+function updateQuantity() {
+  let cartQuantity = 0;
+
+  cart.forEach((cartItem) => {
+    cartQuantity += cartItem.quantity;
+  });
+
+  document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
+}
+
 document.querySelector(".js-products-grid").innerHTML = productsHTML;
 
 document.querySelectorAll(".js-add-to-card").forEach((button) => {
   button.addEventListener("click", () => {
-    const { productId, productName} = button.dataset;
-
-    let matchingItem;
-
-    cart.forEach((item) => {
-      if (productId=== item.productId) {
-        matchingItem = item;
-      }
-    });
-
-    if (matchingItem) {
-      matchingItem.quantity += 1;
-    } else {
-      cart.push({
-        productId: productId,
-        quantity: 1,
-      });
-    }
-    
-    let cartQuantity = 0;
-
-
-    cart.forEach((item) => {
-      cartQuantity += item.quantity;
-    })
-
-    document.querySelector('.js-cart-quantity')
-      .innerHTML = cartQuantity;
-
+    const { productId, productName } = button.dataset;
+    addToCart(productId);
+    updateQuantity();
   });
 });
