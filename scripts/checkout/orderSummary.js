dc713dc3c1862a3c6a45bd26aca7908ffbfe3023
Your Name <22120427@student.hcmus.edu.vn>
2026-06-09 20:47:25 +0700

    feat: make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -3,9 +3,7 @@ import { products, getProduct } from "../../data/products.js";
 import { formatCurrency } from "../utils/money.js";
 import dayjs from "https://unpkg.com/supersimpledev@8.5.0/dayjs/esm/index.js";
 import { deliveryOptions, getDeliveryOption } from "../../data/deliveryOption.js";
-
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -104,6 +102,7 @@ export function renderOrderSummary() {
       );
 
       container.remove();
+      renderPaymentSummary();
     });
   });
 
@@ -112,6 +111,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
