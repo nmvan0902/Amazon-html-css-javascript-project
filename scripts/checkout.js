8087a00f0436428d9d16e0ec253ba6fdfbded546
Your Name <22120427@student.hcmus.edu.vn>
2026-06-11 22:00:43 +0700

    feat: load products from the backend

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,6 +1,9 @@
+import { loadProducts } from "../data/products.js";
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
-import '../data/backend-practice.js'
+// import '../data/backend-practice.js'
 
-renderOrderSummary();
-renderPaymentSummary();
+loadProducts(() => {
+  renderOrderSummary();
+  renderPaymentSummary();
+});
